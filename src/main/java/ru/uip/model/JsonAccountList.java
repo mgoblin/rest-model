23dd0fdabc73286bacfd760463030c51d1378a8f
Daniil <davydovdanya18@gmail.com>
2020-07-28 17:02:42 +0300

    added JsonAccountList

---
+++ src/main/java/ru/uip/model/JsonAccountList.java
@@ -0,0 +1,24 @@
+package ru.uip.model;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement(name="accounts")
+@XmlType(propOrder = {"accounts"})
+@XmlAccessorType(XmlAccessType.FIELD)
+public class JsonAccountList {
+    @XmlElement(name="account")
+    private List<JsonAccount> accounts;
+
+    public JsonAccountList(List<JsonAccount> accounts) {
+        this.accounts = accounts;
+    }
+
+    public JsonAccountList() {
+    }
+
+    public List<JsonAccount> getAccounts() {
+        return accounts;
+    }
+}
